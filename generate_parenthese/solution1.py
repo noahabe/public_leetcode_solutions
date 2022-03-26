6022305c6ff0cd275efbd278675fccb5818463e0
noahabe <noahabe64@gmail.com>
2022-03-26 14:11:51 +0300

    recursive solution for generate parentheses

--- generate_parenthese/solution1.py
+++ generate_parenthese/solution1.py
@@ -1,17 +1,25 @@
+from typing import List 
+
 class Solution(object):
     def generateParenthesis(self, n):
         return self.generatePrefixesOfAWellFormedParentheses(n, n)
 
-    def generatePrefixesOfAWellFormedParentheses(self, openCount, closedCount):
-        if (closedCount == 0):
-            return ['(' * openCount]
-        if openCount == closedCount:
+    def generatePrefixesOfAWellFormedParentheses(self, numberOfOpenBracketsIUse: int, numberOfClosedBracketsIUse: int) -> List[str]:
+        assert(numberOfOpenBracketsIUse >= numberOfClosedBracketsIUse) # by theorem 4 
+
+        if (numberOfClosedBracketsIUse == 0):
+            return ['(' * numberOfOpenBracketsIUse]
+
+        if numberOfOpenBracketsIUse == numberOfClosedBracketsIUse:
+            # then this function returns well-formed parentheses
+            # thus by theorem 2 I know that all of the strings in the list will end in a closing bracket. 
             res = [
-                i + ")"  for i in self.generatePrefixesOfAWellFormedParentheses(openCount, closedCount-1)]
+                i + ")"  for i in self.generatePrefixesOfAWellFormedParentheses(numberOfOpenBracketsIUse, numberOfClosedBracketsIUse-1)]
             return res
-        else:
+            
+        elif numberOfOpenBracketsIUse > numberOfClosedBracketsIUse:
             res = [
-                i + "(" for i in self.generatePrefixesOfAWellFormedParentheses(openCount-1, closedCount)]
+                i + "(" for i in self.generatePrefixesOfAWellFormedParentheses(numberOfOpenBracketsIUse-1, numberOfClosedBracketsIUse)]
             res2 = [
-                i + ")" for i in self.generatePrefixesOfAWellFormedParentheses(openCount, closedCount-1)]
+                i + ")" for i in self.generatePrefixesOfAWellFormedParentheses(numberOfOpenBracketsIUse, numberOfClosedBracketsIUse-1)]
             return res + res2
